d8c327e04ff1522f30f775dc77bec325b374f855
Razzzan <126163074+Razzzan@users.noreply.github.com>
2023-05-05 21:54:06 +0300

    Update scripts.php

--- Pages/Views/Shared/scripts.php
+++ Pages/Views/Shared/scripts.php
@@ -12,3 +12,32 @@ function toggleHeaderScript() {
 }
 
 ?>
+<?php
+
+function sending_budget() {
+	?>
+	<script>
+    $(document).ready(function() {
+        $('#budget-form').submit(function(event) {
+            event.preventDefault(); // Prevent form from submitting normally
+            var budget = $('#budget').val();
+            $.ajax({
+                url: 'recipes.php',
+                type: 'GET',
+                data: {budget: budget},
+                success: function(response) {
+                    // Handle successful response from the server
+                    alert('Budget submitted successfully');
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    // Handle errors
+                    alert('Error submitting budget: ' + textStatus + ' - ' + errorThrown);
+                }
+            });
+        });
+    });
+	</script>
+	<?php
+}
+
+?>
