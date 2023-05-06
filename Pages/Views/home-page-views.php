7488406b133cea43a6bfbb8d0364619d5825bfb7
MahmoudJLB <mahmoud.j.eschool@gmail.com>
2023-05-06 22:13:58 +0300

    Add slideshow
    Finalized basic slideshow structure and style

--- Pages/Views/home-page-views.php
+++ Pages/Views/home-page-views.php
@@ -7,9 +7,13 @@
 			</div>
 			<div class="slideshow-arrow left" onclick="moveSlideshowLeft()"></div>
 			<div class="slideshow-arrow right" onclick="moveSlideshowRight()"></div>
+			<div id="slider">
+			</div>
+			<!--
 			<div id="view">
 				<a href="#offers">View Current Offers!</a>
 			</div>
+			-->
 		</div>
 		<?php
 	}
@@ -19,9 +23,21 @@
 			<script>
 				const slideshow = document.getElementById("slideshow");
 				const carousel = document.getElementById("slideshow-carousel");
-				let image_srcs = ["AvengersAgeOfUltron.jpg", "BreakingBad.jpg", "CaptainAmericaTheFirstAvenger.jpg", "CaptainMarvel.jpg", "FRIENDS.png", "GuardiansOfTheGalaxy.webp", "GuardiansOfTheGalaxy2.webp", "IronMan.jpg", "IronMan2.jpg", "Manifest.jpg", "TheIncredibleHulk.webp"];
+
 				const size = 10;
 				let index = 0;
+				// Generate slider
+				const slider = document.getElementById("slider");
+				for (let i = 0; i < size; i++) {
+					const circle = document.createElement("div");
+					circle.setAttribute("tabindex", 0);
+					circle.setAttribute("onclick", `moveSlideshowHere(${i})`);
+					circle.classList.add("circle");
+					slider.appendChild(circle);
+				}
+				const circles = document.querySelectorAll("#slider .circle");
+				// Generate slideshow images
+				const image_srcs = ["AvengersAgeOfUltron.jpg", "BreakingBad.jpg", "CaptainAmericaTheFirstAvenger.jpg", "CaptainMarvel.jpg", "FRIENDS.png", "GuardiansOfTheGalaxy.webp", "GuardiansOfTheGalaxy2.webp", "IronMan.jpg", "IronMan2.jpg", "Manifest.jpg", "TheIncredibleHulk.webp"];
 				for (let i = 0; i < size; i++) {
 					const img = document.createElement("div");
 					img.classList.add("slideshow-image");
@@ -37,23 +53,33 @@
 	function moveSlideshowScript() {
 		?>
 		<script>
-			let moveSlideshowInterval = setInterval(moveSlideshowRight, 3000);
+			circles[0].classList.add("fill");
+			const updateSpeed = 3500;
+			let moveSlideshowInterval = setInterval(moveSlideshowRight, updateSpeed);
 			function moveSlideshowLeft() {
+				circles[index].classList.remove("fill");
 				index -= 1;
 				if (index < 0) index = size-1;
 				moveSlideshow();
 			}
 			function moveSlideshowRight() {
+				circles[index].classList.remove("fill");
 				index = (index+1)%size;
+				moveSlideshow();
+			}
+			function moveSlideshowHere(current_index) {
+				circles[index].classList.remove("fill");
+				index = current_index;
 				moveSlideshow();
 			}
 			function moveSlideshow() {
 				clearInterval(moveSlideshowInterval);
+				circles[index].classList.add("fill");
 				for (let i = 0; i < size; i++) {
 					const offset = index - i;
 					images[i].style.right = `${offset*100}%`;
 				}
-				moveSlideshowInterval = setInterval(moveSlideshowRight, 3000);
+				moveSlideshowInterval = setInterval(moveSlideshowRight, updateSpeed);
 			}
 		</script>
 		<?php
