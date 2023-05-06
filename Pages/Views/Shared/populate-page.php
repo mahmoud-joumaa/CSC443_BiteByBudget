5b827985ec6f42d18cf72abb830135e54e6c6b58
MahmoudJLB <mahmoud.j.eschool@gmail.com>
2023-05-06 13:40:26 +0300

    Update populate-page.php
    Added toggleHeaderScript() to populate-page.php

--- Pages/Views/Shared/populate-page.php
+++ Pages/Views/Shared/populate-page.php
@@ -20,6 +20,17 @@
 		<?php
 	}
 
+	function toggleHeaderScript() {
+		?>
+			<script>
+				function toggleHeader() {
+					const menu = document.getElementById("burger-menu");
+					menu.classList.toggle("expand");
+				}
+			</script>
+		<?php
+	}
+
 	function populateFooterView() {
 		?>
 			<div id="footer">
