5215d8f653efc73e524612c8a439465518997f3e
MahmoudJLB <mahmoud.j.eschool@gmail.com>
2023-05-07 14:20:10 +0300

    Add Meet The Team

--- Pages/about-us.php
+++ Pages/about-us.php
@@ -1,30 +1,47 @@
+<?php
+
+require_once "Views/Shared/populate-page.php";
+
+require_once "Views/about-us-views.php";
+
+?>
+
 <html>
+
 	<head>
 		<!-- Title Bar -->
-		<title>BBB | About Us</title>
+		<title>BBB | Home Page</title>
 		<!-- Linked Styles -->
-		<link href="Styles/Shared/reset.css" rel="stylesheet" type="text/css">
-		<link href="Styles/Shared/icons.css" rel="stylesheet" type="text/css">
-		<link href="Styles/Shared/header.css" rel="stylesheet" type="text/css">
-		<link href="Styles/Shared/footer.css" rel="stylesheet" type="text/css">
+		<link href="../Styles/Shared/reset.css" rel="stylesheet" type="text/css">
+		<link href="../Styles/Shared/icons.css" rel="stylesheet" type="text/css">
+		<link href="../Styles/Shared/header.css" rel="stylesheet" type="text/css">
+		<link href="../Styles/Shared/footer.css" rel="stylesheet" type="text/css">
 		<link href="../Styles/about-us.css" rel="stylesheet" type="text/css">
-		<!-- Linked Scripts -->
-		<script src="Scripts/Shared/header.js"></script>
 	</head>
+
 	<body>
-		<!-- Header -->
+		<!-- Menu Nav Bar -->
+		<?php populateHeaderView(); ?>
 		<!-- Main -->
 		<div id="main">
-			<div class="team-member">
-				<div class="image">
-					<div class="quote">
-						<span>"Meow"</span>
-					</div>
-				</div>
-				<span>Blob Cat</span><br>
-				<span>Programmer & Cat</span>
+			<h2>Meet The Team</h2>
+			<div id="team-wrapper">
+				<?php
+					$members = getMembers();
+					$team_size = count($members);
+					for ($i = 0; $i < $team_size; $i++) {
+						populateTeamMemberView($members[$i]);
+					}
+				?>
 			</div>
 		</div>
 		<!-- Footer -->
+		<?php populateFooterView(); ?>
 	</body>
+
+	<!-- Linked Scripts -->
+	<?php
+		toggleHeaderScript();
+	?>
+
 </html>
