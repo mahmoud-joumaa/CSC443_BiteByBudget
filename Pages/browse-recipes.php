60371303bc8e9ceb9c72f5753b7a99f2ca9c1fd3
MahmoudJLB <mahmoud.j.eschool@gmail.com>
2023-05-11 01:02:17 +0300

    Fix merging

--- Pages/browse-recipes.php
+++ Pages/browse-recipes.php
@@ -1,8 +1,7 @@
 <?php
 
 require_once "Views/Shared/populate-page.php";
-require_once "Views/Shared/scripts.php";
-require_once "Views/home-page-views.php";
+
 require_once "Views/browse-recipes-page-view.php";
 
 
@@ -10,38 +9,39 @@ require_once "Views/browse-recipes-page-view.php";
 
 <html>
 
-		<head>
-			<!-- Title Bar -->
-			<title>BBB | Browse Recipes</title>
+	<head>
+		<!-- Title Bar -->
+		<title>BBB | Browse Recipes</title>
 			<!-- Linked Styles -->
 			<link href="../Styles/Shared/reset.css" rel="stylesheet" type="text/css">
-			<link href="../Styles/Shared/icons.css" rel="stylesheet" type="text/css">
 			<link href="../Styles/Shared/header.css" rel="stylesheet" type="text/css">
 			<link href="../Styles/Shared/footer.css" rel="stylesheet" type="text/css">
-            <script src="../Libraries/jquery.min.js"></script>
-            <?php echo loadScripts() ?>
-			<!-- Linked Scripts -->
+	</head>
 
-            
-			<?php
-				toggleHeaderScript();
-			?>
-		</head>
-
-		<body>
-			<!-- Menu Nav Bar -->
-			<?php populateHeaderView(); ?>
+	<body>
+		<!-- Loader -->
+		<?php populateLoaderView(); ?>
+		<!-- Menu Nav Bar -->
+		<?php populateHeaderView(); ?>
 			<!-- Main -->
 			<div id="main">
-                <div id="step-1"> </div>
-                <div id="step-2"> </div>
-                <div id="step-3"> </div>
-                <div id="step-4"> </div>
-				<div id="step-4-half"> </div>
-                <div id="step-5"> </div>
+				<div id="step-1"> </div>
+				<div id="step-2"> </div>
+				<div id="step-3"> </div>
+				<div id="step-4"> </div>
+				<div id="step-4-half"></div>
+				<div id="step-5"> </div>
 			</div>
 			<!-- Footer -->
 			<?php populateFooterView(); ?>
 		</body>
 
-	</html>
+	<!-- Linked Scripts -->
+	<script src="../Libraries/jquery.min.js"></script>
+	<?php
+		animateLoaderScript();
+		toggleHeaderScript();
+		echo loadScripts();
+	?>
+
+</html>
